1706f51f8a64dfd692188f041710a4d08018227d
Z3EAl <ashray.praneeth@gmail.com>
2024-02-17 00:56:54 -0700

    added comments to code

--- Group Anagrams/solution.py
+++ Group Anagrams/solution.py
@@ -1,16 +1,29 @@
+
+# ! Initializing Variables
+# We start by initializing an empty unordered map called mp (short for map), which will store the groups of anagrams.
+# Grouping Anagrams
+# We iterate through each word in the input vector strs. Let's take the first word, "eat", as an example.
+
+# ! Sorting the Word
+# We create a string variable called word and assign it the value of the current word ("eat" in this case).
+# Next, we sort the characters in word using the sort() function. After sorting, word becomes "aet".
+
+# ! Grouping the Anagram
+# We insert word as the key into the mp unordered map using mp[word], and we push the original word ("eat") into the vector associated with that key using mp[word].append(x), where x is the current word.
+# Since "aet" is a unique sorted representation of all the anagrams, it serves as the key in the mp map, and the associated vector holds all the anagrams.
+# Creating the Result
+# We initialize an empty vector called ans (short for answer) to store the final result.
+
+# We iterate through each key-value pair in the mp map using a for loop. For each pair, we append the vector of anagrams (x[1]) into the ans vector.
+
+
+from collections import defaultdict
 class Solution:
-    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        if len(strs)==1:
-            return [strs]
-        seen={}
-        for i,val in enumerate(strs):
-            temp=[]
-            sp="".join(sorted(list(val)))
-            if sp in seen:
-                temp=seen[sp]
-                temp.append(strs[i])
-                seen[sp]=temp
-            else:
-                seen[sp]=[strs[i]]
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
         
-        return list(seen.values())
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        
+        return list(anagram_map.values())
